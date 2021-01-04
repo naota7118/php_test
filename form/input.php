e4dfeacb835ec72caa4a7367074c508fc73e1ba1
naota7118 <naota7118@gmail.com>
2021-01-04 16:33:40 +0900

    update input.php

--- form/input.php
+++ form/input.php
@@ -3,10 +3,10 @@
 session_start();
 
 // 空ではなかったらechoを表示する
-if(!empty($_SESSION)){
+if(!empty($_POST)){
   echo '<pre>';
   // スーパーグローバル変数['キー']という形で取得できる
-  var_dump ($_SESSION);
+  var_dump ($_POST);
   echo '</pre>';
 }
 header('X-FRAME-OPTIONS:DENY');
@@ -47,13 +47,39 @@ if(!empty($_POST['btn_submit'])){
 メールアドレス
 <?php echo h($_POST['email']) ;?>
 <br>
+ホームページ
+<?php echo h($_POST['url']) ;?>
+<br>
+性別
+<?php
+  if($_POST['gender'] === '0'){ echo '男性'; }
+  if($_POST['gender'] === '1'){ echo '女性'; }
+?>
+<br>
+年齢
+<?php
+  if($_POST['age'] === '1'){ echo '〜19歳'; }
+  if($_POST['age'] === '2'){ echo '20歳〜29歳'; }
+  if($_POST['age'] === '3'){ echo '30歳〜39歳'; }
+  if($_POST['age'] === '4'){ echo '40歳〜49歳'; }
+  if($_POST['age'] === '5'){ echo '50歳〜59歳'; }
+  if($_POST['age'] === '6'){ echo '60歳〜'; }
+?>
+<br>
+お問い合わせ内容
+<?php echo h($_POST['contact']) ;?>
+
+<br>
 <input type="submit" name="back" value="戻る">
 
 <input type="submit" name="btn_submit" value="送信する">
 <!-- hiddenで設定すると表には表示されないがデータとしては持っている -->
 <input type="hidden" name="your_name" value="<?php echo h($_POST['your_name']) ;?>">
 <input type="hidden" name="email" value="<?php echo h($_POST['email']) ;?>">
-<input type="hidden" name="csrf" value="<?php echo h($_POST['csrf']) ;?>">
+<input type="hidden" name="url" value="<?php echo h($_POST['url']) ;?>">
+<input type="hidden" name="gender" value="<?php echo h($_POST['gender']) ;?>">
+<input type="hidden" name="age" value="<?php echo h($_POST['age']) ;?>">
+<input type="hidden" name="contact" value="<?php echo h($_POST['contact']) ;?>">
 
 </form>
 
@@ -91,6 +117,42 @@ $token = $_SESSION['csrfToken'];
 メールアドレス
 <input type="email" name="email" value="<?php if(!empty($_POST['email'])){echo h($_POST['email']) ;} ?>">
 <br>
+ホームページ
+<input type="url" name="url" value="<?php if(!empty($_POST['url'])){echo h($_POST['url']) ;} ?>">
+<br>
+性別
+
+<input type="radio" name="gender" value="0"
+<?php if(!empty($_POST['gender']) && $_POST['gender'] === '0')
+{ echo 'checked'; } ?>
+>男性
+
+<input type="radio" name="gender" value="1"
+<?php if(!empty($_POST['gender']) && $_POST['gender'] === '1')
+{ echo 'checked'; } ?>
+>女性
+
+<br>
+年齢
+<select name="age">
+  <option value="">選択してください</option>
+  <option value="1">〜19歳</option>
+  <option value="2">20歳〜29歳</option>
+  <option value="3">30歳〜39歳</option>
+  <option value="4">40歳〜49歳</option>
+  <option value="5">50歳〜59歳</option>
+  <option value="6">60歳</option>
+</select>
+<br>
+お問い合わせ内容
+<textarea name="contact">
+<?php if(!empty($_POST['contact'])){echo h($_POST['contact']) ;} ?>
+</textarea>
+<br>
+<input type="checkbox" name="caution" value="1">注意事項にチェックする
+<br>
+注意事項のチェック
+
 <input type="submit" name="btn_confirm" value="確認する">
 <input type="hidden" name="csrf" value="<?php echo $token;?>">
 
