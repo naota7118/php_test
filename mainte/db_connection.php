d98aec46a2c04897ef8025cb10416ab34890356a
naota7118 <naota7118@gmail.com>
2021-01-04 18:57:41 +0900

    setting sql injection   countermeasures

--- mainte/db_connection.php
+++ mainte/db_connection.php
@@ -9,7 +9,12 @@ const DB_PASSWORD = 'password123';
 
 // これはDB接続する時のお決まりの書き方
 try{
-  $pdo = new PDO(DB_HOST, DB_USER, DB_PASSWORD);
+  // 4つ目の引数に配列の形でオプションを付けられる
+  $pdo = new PDO(DB_HOST, DB_USER, DB_PASSWORD, [
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //DBから帰ってくる値を連想配列の形で取得する
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, //例外を表示
+    PDO::ATTR_EMULATE_PREPARES => false, //SQLインジェクション対策
+  ]);
   echo '接続成功';
 } catch(PDOException $e){
   echo '接続失敗' . $e->getMessage() . "\n";
